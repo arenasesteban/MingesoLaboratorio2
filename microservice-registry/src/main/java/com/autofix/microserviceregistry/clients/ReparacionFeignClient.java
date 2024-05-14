0c3f18fe6dcbfb263d280038f156a6fd90fb3f3a
arenasesteban <esteban.arenas.a@usach.cl>
2024-05-14 17:09:53 -0400

    [Esteban] Correción query para buscar por patente y menos al valor de id registro

--- microservice-registry/src/main/java/com/autofix/microserviceregistry/clients/ReparacionFeignClient.java
+++ microservice-registry/src/main/java/com/autofix/microserviceregistry/clients/ReparacionFeignClient.java
@@ -2,7 +2,6 @@ package com.autofix.microserviceregistry.clients;
 
 import com.autofix.microserviceregistry.configurations.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- microservice-registry/src/main/java/com/autofix/microserviceregistry/repositories/RegistroRepository.java
+++ microservice-registry/src/main/java/com/autofix/microserviceregistry/repositories/RegistroRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
-    @Query("SELECT r.id_registro FROM Registro r WHERE r.id_registro < :id_registro")
+    @Query("SELECT r.id_registro FROM Registro r WHERE r.patente = :patente AND r.id_registro < :id_registro")
     List<Long> buscarPorPatenteYMenorAId_registro(String patente, Long id_registro);
 
     @Query("SELECT DISTINCT r.patente FROM Registro r")
